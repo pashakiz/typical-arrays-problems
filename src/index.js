f58888cc3f30ae6621c69bd362513ffb1f52547c
pashakiz <pashakiz@gmail.com>
2021-08-22 15:30:22 +0300

    feat: implement functions (min, max, avg)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length === 0) return 0
+    return array.reduce((min, el) => el < min ? min = el : min, array[0])
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length === 0) return 0
+    return array.reduce((max, el) => el > max ? max = el : max, array[0])
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0) return 0
+    return array.reduce((sum, el) => sum + el, 0) / array.length
 }
